2d4a69738b9f2660cfae86c575942924f4b902cd
govardhan <govardhanaaleswara88@gmail.com>
2021-11-08 21:00:13 +0530

    search function added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
+import Alert from './components/layouts/Alert';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null,
   }
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_Client_ID}&client_secret=${process.env.REACT_APP_Client_Secrets}`);
+  //   this.setState({ users: res.data, loading: false });
+  // }
+  // search github users
+  searchUsers = async (e) => {
+    console.log(e);
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_Client_ID}&client_secret=${process.env.REACT_APP_Client_Secrets}`);
-    // console.log(res.request.responseURL)
-    this.setState({ users: res.data, loading: false });
+    const res = await axios.get(`https://api.github.com/search/users?q=${e}&client_id=${process.env.REACT_APP_Client_ID}&client_secret=${process.env.REACT_APP_Client_Secrets}`);
+    console.log(res.data.items)
+    this.setState({ users: res.data.items, loading: false });
+  }
+  //clear users
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  }
+  //set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+
   }
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
+        <Alert alert={this.state.alert} />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search setAlert={this.setAlert} showClear={this.state.users.length > 0 ? true : false} searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -10,7 +10,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
             />
             <h3>{login}</h3>
             <div>
-                <a href={html_url} target="_blank" className="btn btn-dark btn-sm my-1">More</a>
+                <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm my-1">More</a>
             </div>
         </div>
     )
